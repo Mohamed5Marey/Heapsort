81438aea2445b06097aed0a508cb840a24711e2e
Mohamed Marey <121446389+Mohamed5Marey@users.noreply.github.com>
2024-12-24 23:23:27 +0200

    Update HeapSort Implementation.cpp

--- HeapSort Implementation.cpp
+++ HeapSort Implementation.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 
 void heapify(int arr[], int n, int i) {
@@ -40,7 +40,7 @@ void printArray(int arr[], int n) {
 }
 
 int main() {
-    int arr[] = { 12, 11, 13, 5, 6, 7 };
+    int arr[] = {12, 11, 13, 5, 6, 7};
     int n = sizeof(arr) / sizeof(arr[0]);
 
     heapSort(arr, n);
